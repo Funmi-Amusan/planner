52d2fed1a19655920f94b77a746fa0753e54f79b
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-08-26 23:46:05 +0100

    adding nav items to sidebar

--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -1,15 +1,51 @@
-import { IoCalendarOutline } from "react-icons/io5";
+import {
+    IoCalendarOutline,
+    IoHomeSharp,
+    IoCaretForwardSharp,
+    IoCalendar,
+    IoMail,
+    IoTimer
+} from "react-icons/io5";
 
 const sidebar = () => {
     return (
-        <div className=" flex flex-col items-center h-screen text-white bg-slate-500 w-64">
+        <div className=" flex flex-col items-center h-screen text-white bg-slate-500 w-64 p-6">
             {/* header */}
-            <div className=" flex items-center">
+            <div className=" flex items-center justify-start text-start">
                 <IoCalendarOutline />
                 <h1>Planner</h1>
             </div>
             {/* nav */}
-            <div></div>
+            <div className=" w-full">
+                <nav className=" capitalize flex justify-between ">
+                    <div className=" items-center flex gap-2">
+                        <IoHomeSharp />
+                        <h1>Home</h1>
+                    </div>
+                    <IoCaretForwardSharp />
+                </nav>
+                <nav className=" capitalize flex justify-between ">
+                    <div className=" items-center flex gap-2">
+                        <IoCalendar />
+                        <h1>calendar</h1>
+                    </div>
+                    <IoCaretForwardSharp />
+                </nav>
+                <nav className=" capitalize flex justify-between ">
+                    <div className=" items-center flex gap-2">
+                        <IoMail />
+                        <h1>inbox</h1>
+                    </div>
+                    <IoCaretForwardSharp />
+                </nav>
+                <nav className=" capitalize flex justify-between ">
+                    <div className=" items-center flex gap-2">
+                        <IoTimer />
+                        <h1>productivity</h1>
+                    </div>
+                    <IoCaretForwardSharp />
+                </nav>
+            </div>
 
             {/* favourite */}
             <div></div>
