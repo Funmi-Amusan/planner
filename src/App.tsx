ca09d08b25848eeca2d2a3e003f16f9a1d9971f1
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-08-26 23:34:36 +0100

    sidebar header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Header from "./layout/header";
+import Sidebar from "./layout/sidebar";
 
 const App = () => {
     return (
         <div className=" bg-[#1c1f25]">
+            <Sidebar />
             <Header />
         </div>
     );
--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { IoCalendarOutline } from "react-icons/io5";
 
 const sidebar = () => {
-    return <div>sidebar</div>;
+    return (
+        <div className=" flex flex-col items-center h-screen text-white bg-slate-500 w-64">
+            {/* header */}
+            <div className=" flex items-center">
+                <IoCalendarOutline />
+                <h1>Planner</h1>
+            </div>
+            {/* nav */}
+            <div></div>
+
+            {/* favourite */}
+            <div></div>
+
+            {/* workspace */}
+            <div></div>
+            {/* upgrade */}
+            <div></div>
+
+            {/* log out */}
+            <div></div>
+        </div>
+    );
 };
 
 export default sidebar;
