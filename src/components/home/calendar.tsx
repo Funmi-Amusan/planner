64f6a7da4053dd64714fe546ecc534d723ef782d
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-08-28 22:39:38 +0100

    adding calendar dates

--- src/components/home/calendar.tsx
+++ src/components/home/calendar.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const calendar = () => {
-    return <div>calendar</div>;
+    return (
+        <div className=" text-white">
+            <div className=" flex justify-between ">
+                <h1>Calendar</h1>
+                <p className=" underline">See all</p>
+            </div>
+            <div>
+                <p>Today</p>
+                <h3>Monday, 26 June 2024</h3>
+            </div>
+            <div>
+                <div>
+                    <div className=" flex flex-col gap-2 border border-blue-600 rounded-xl p-2 w-fit text-center">
+                        <h3>Mon</h3>
+                        <p>26</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default calendar;
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Tasks from "./tasks";
+import Calendar from "./calendar";
 
 const home = () => {
     return (
-        <div className=" w-full h-full ">
-            <div className="grid grid-cols-2 h-screen">
+        <div className=" w-full h-full  work-sans ">
+            <div className="grid grid-cols-2 gap-5 h-screen">
                 <div>
                     <div className=" h-3/5 ">
                         <Tasks />
@@ -14,7 +15,9 @@ const home = () => {
 
                 <div>
                     <div>summary</div>
-                    <div>calendar</div>
+                    <div>
+                        <Calendar />
+                    </div>
                 </div>
             </div>
         </div>
