f65ad6ec6b280f916923184079bbe5f7691157bf
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-08-31 23:19:14 +0100

    plans page

---
+++ src/components/plans/choosePlan.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const choosePlan = () => {
+    return (
+        <div>
+            <div>
+                <h1>Choose your plan</h1>
+                <div>tabs</div>
+            </div>
+
+            <div>
+                <h1>map through plans from mock data</h1>
+            </div>
+        </div>
+    );
+};
+
+export default choosePlan;
