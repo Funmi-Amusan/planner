0a3aa13a62641f908654014fd7d76d6e98b47343
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-09-02 23:31:47 +0100

    adding pagination

--- src/components/home/tasks.tsx
+++ src/components/home/tasks.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import * as React from "react";
+import Pagination from "@mui/material/Pagination";
 import TaskItem from "./taskItem";
 import { Tasks } from "../../mockData/taskData";
+import Stack from "@mui/material/Stack";
 
 const tasks = () => {
+    const whiteTextStyle = {
+        "& .MuiPaginationItem-root": {
+            color: "white"
+        }
+    };
+
     return (
         <div className=" bg-black border border-slate-700 text-white work-sans rounded-xl p-5">
             <div className=" flex justify-between">
@@ -17,6 +25,19 @@ const tasks = () => {
                     appImage={task.appImage}
                 />
             ))}
+            <div className=" flex justify-end place-items-end place-self-end mt-2">
+                <div className=" w-fit ">
+                    <Stack spacing={2}>
+                        <Pagination
+                            count={10}
+                            color="primary"
+                            variant="text"
+                            shape="rounded"
+                            sx={whiteTextStyle}
+                        />
+                    </Stack>
+                </div>
+            </div>
         </div>
     );
 };
