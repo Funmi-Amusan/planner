b563938a3305f2e7c130453dce768726a2fd7fe2
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-09-03 22:16:34 +0100

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import Home from "./components/home/home";
+// import Home from "./components/home/home";
 import Layout from "./components/layout/layout";
 
 const App = () => {
     return (
         <div>
-            <Layout>
-                <Home />
-            </Layout>
+            <Layout>{/*  */}</Layout>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/home";
+import Prices from "./pages/prices";
+import Schedule from "./pages/schedule";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/prices",
+        element: <Prices />
+    },
+    {
+        path: "/schedule",
+        element: <Schedule />
+    }
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
