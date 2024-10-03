09d0eb34bbcd418d89be84036fa752667ffead83
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-10-03 23:55:18 +0100

    removing toasts

--- src/components/dashboard/stepper.tsx
+++ src/components/dashboard/stepper.tsx
@@ -1,57 +1,12 @@
 import React, { useState, useRef } from "react";
 import { Steps } from "primereact/steps";
-import { Toast } from "primereact/toast";
 
 export const StepsDemo = () => {
     const [activeIndex, setActiveIndex] = useState(1);
     const toast = useRef(null);
-    const items = [
-        {
-            label: "Personal",
-            command: (event: { item: { label: any } }) => {
-                toast.current.show({
-                    severity: "info",
-                    summary: "First Step",
-                    detail: event.item.label
-                });
-            }
-        },
-        {
-            label: "Seat",
-            command: (event: { item: { label: any } }) => {
-                toast.current.show({
-                    severity: "info",
-                    summary: "Seat Selection",
-                    detail: event.item.label
-                });
-            }
-        },
-        {
-            label: "Payment",
-            command: (event: { item: { label: any } }) => {
-                toast.current.show({
-                    severity: "info",
-                    summary: "Pay with CC",
-                    detail: event.item.label
-                });
-            }
-        },
-        {
-            label: "Confirmation",
-            command: (event: { item: { label: any } }) => {
-                toast.current.show({
-                    severity: "info",
-                    summary: "Last Step",
-                    detail: event.item.label
-                });
-            }
-        }
-    ];
 
     return (
         <div className="steps-demo">
-            <Toast ref={toast}></Toast>
-
             <div className="card">
                 <h5>Basic</h5>
                 <Steps model={items} />
