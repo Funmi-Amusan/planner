c92dab14d9b8d9cde2692016e9e7e81660747a32
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-10-05 00:19:18 +0100

    adding react big calendar

--- src/components/home/calendar.tsx
+++ src/components/home/calendar.tsx
@@ -14,26 +14,6 @@ const calendar = () => {
                 <p>Today</p>
                 <h3>Monday, 26 June 2024</h3>
             </div>
-            <div>
-                <div>
-                    {/* <div className=" flex flex-col gap-2 border border-blue-600 rounded-xl p-2 w-fit text-center">
-                        <h3>Mon</h3>
-                        <p>26</p>
-                    </div> */}
-
-                    <Calendar
-                        value={date}
-                        // onChange={(e) => setDate(e)}
-                    ></Calendar>
-
-                    <div>
-                        <div className="bg-slate-600 w-full border-l-4 rounded-md border-blue-600 p-3">
-                            <h3>Task Planning</h3>
-                            <p>Review daily tasks and set priorities</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 };
