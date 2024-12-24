3499b8634ec6618d3f1ce8b449e6c35a40afd721
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-12-24 07:06:39 +0100

    delete all files

--- src/components/dashboard/stepper.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useRef } from "react";
-import { Steps } from "primereact/steps";
-
-export const StepsDemo = () => {
-    const [activeIndex, setActiveIndex] = useState(1);
-    const toast = useRef(null);
-
-    return <div className="steps-demo"></div>;
-};
